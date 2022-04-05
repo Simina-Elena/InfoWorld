2de92756fab5a73ff3c8307faf487cdee32db74a
Simina Tabac <siminatabac@yahoo.com>
2022-04-05 21:26:11 +0300

    working to refactor methods in doctors.service

--- modules/doctors/doctors.routes.js
+++ modules/doctors/doctors.routes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const {receiveEntries, login} = require('./doctors.controller')
 const router = express.Router()
+const {authenticateToken} = require('./doctors.service')
 
-router.route('/').post(receiveEntries)
+router.route('/').post([authenticateToken, receiveEntries])
 router.route('/login').post(login)
 
 module.exports = router
--- modules/doctors/doctors.service.js
+++ modules/doctors/doctors.service.js
@@ -3,7 +3,7 @@ const csvToJson = require("csvtojson");
 require('dotenv').config()
 const ids = []
 
-const authenticateToken = (req, res) => {
+exports.authenticateToken = (req, res, next) => {
     const authHeader = req.headers['x-vamf-jwt'];
     const token = authHeader && authHeader.split(' ')[1];
     if (token === null) return res.sendStatus(401);
@@ -16,6 +16,7 @@ const authenticateToken = (req, res) => {
             res.status(401).send('Access denied for that role.')
         }
     })
+    next()
 }
 
 const checkFacilitiesForJson = (req, facility) => {
@@ -27,40 +28,49 @@ const checkFacilitiesForJson = (req, facility) => {
     req.facilityFlag = facilities.some(el => user.facility.includes(el)) === true;
 }
 
+const checkIdAndResourceType = (body, user, res) => {
+    if (body.id === undefined) {
+        return res.status(400).send('Id is not provided!')
+    }
+
+    if (body.resourceType !== 'Practitioner') {
+        return res.status(400).send('Wrong resource type!')
+    }
+
+    if (ids.includes(body.id)) {
+        return res.status(400).send('You are using the same id!')
+    } else {
+        ids.push(body.id)
+    }
+    logInformation(body, user, res)
+}
+
+const logInformation = (body, user, res) => {
+    for (let i = 0; i < body.name.length; i++) {
+        console.log('Family name: ' + body.name[i].family)
+        console.log('Given name: ' + body.name[i].given)
+        console.log('Full name: ' + body.name[i].text)
+    }
+    for (let i = 0; i < body.facility.length; i++) {
+        if (user.facility.includes(body.facility[i].value)) {
+            console.log(body.facility[i].value + '. ' + 'Facility name: ' + body.facility[i].name
+                + ' --- ' + 'System: ' + body.facility[i].system)
+        }
+    }
+    console.log("-----------------------------------------")
+    return res.status(200).send('Information logged to the console.')
+}
+
 exports.handleEntries = (req, res) => {
-    authenticateToken(req, res)
+    // authenticateToken(req, res)
     if(req.user) {
         const body = req.body
         const user = req.user.user
         if (req.get("Content-Type") === "application/json") {
-            if (body.id === undefined) {
-                return res.status(400).send('Id is not provided!')
-            }
-
-            if (body.resourceType !== 'Practitioner') {
-                return res.status(400).send('Wrong resource type!')
-            }
-
             checkFacilitiesForJson(req, body.facility)
 
             if (body.active === true && req.facilityFlag === true) {
-                if (ids.includes(body.id)) {
-                    return res.status(400).send('You are using the same id!')
-                } else {
-                    ids.push(body.id)
-                }
-                for (let i = 0; i < body.name.length; i++) {
-                    console.log('Family name: ' + body.name[i].family)
-                    console.log('Given name: ' + body.name[i].given)
-                    console.log('Full name: ' + body.name[i].text)
-                }
-                for (let i = 0; i < body.facility.length; i++) {
-                    if (user.facility.includes(body.facility[i].value))
-                        console.log(body.facility[i].value + '. ' + 'Facility name: ' + body.facility[i].name
-                            + ' --- ' + 'System: ' + body.facility[i].system)
-                }
-                res.status(200).send('Information logged to the console.')
-
+                checkIdAndResourceType(body, user, res)
             } else {
                 return res.status(400).send('Status inactive or facility discrepancy!')
             }
@@ -68,9 +78,8 @@ exports.handleEntries = (req, res) => {
         } else if (req.get("Content-Type") === 'text/csv') {
             const data = []
             let content = null
-            let output = {}
+            req.output = {}
             let objForCheck = {}
-            let flag = true
             req.on('data', (chunk) => {
                 data.push(chunk)
             })
@@ -81,39 +90,77 @@ exports.handleEntries = (req, res) => {
                         objForCheck[entry.FamilyName + ' ' + entry.GivenName] = entry.ID
                     })
                     obj.forEach((entry) => {
-                        for (const [key, value] of Object.entries(objForCheck)) {
-                            if (entry.ID === value && entry.FamilyName + ' ' + entry.GivenName !== key) {
-                                flag = false
-                            }
-                        }
-                        if (flag === true) {
-                            if (entry.Active === 'true' && user.facility.includes(entry.FacilityId)) {
-                                if (output[entry.FamilyName + ' ' + entry.GivenName]) {
-                                    output[entry.FamilyName + ' ' + entry.GivenName].push(entry.NameId);
-                                } else {
-                                    output[entry.FamilyName + ' ' + entry.GivenName] = [entry.NameId];
-                                }
-                            }
-                        }
+                        // for (const [key, value] of Object.entries(objForCheck)) {
+                        //     if (entry.ID === value && entry.FamilyName + ' ' + entry.GivenName !== key) {
+                        //         flag = false
+                        //     }
+                        // }
+                        checkObjectsForIdsAndNames(req, objForCheck, entry)
+                        // if (req.sameIdDifNameFlag !== true) {
+                        //     if (entry.Active === 'true' && user.facility.includes(entry.FacilityId)) {
+                        //         if (output[entry.FamilyName + ' ' + entry.GivenName]) {
+                        //             output[entry.FamilyName + ' ' + entry.GivenName].push(entry.NameId);
+                        //         } else {
+                        //             output[entry.FamilyName + ' ' + entry.GivenName] = [entry.NameId];
+                        //         }
+                        //     }
+                        // }
+                        populateOutput(req, user, entry)
                     })
-
-                    if (isEmptyObject(output) === true) {
-                        return res.status(400).send('You are using the same ID with different names!')
-                    } else {
-                        for (const [key, value] of Object.entries(output)) {
-                            let s = key + ': '
-                            for (let i = 0; i < value.length; i++) {
-                                s += value[i] + ', '
-                            }
-                            console.log(s.substring(0, s.length - 2))
-                        }
-                        return res.status(200).send('Information logged to the console.')
-                    }
+                    logInformationCSV(req, res)
+                    // if (isEmptyObject(req.output) === true) {
+                    //     return res.status(400).send('You are using the same ID with different names!')
+                    // } else {
+                    //     for (const [key, value] of Object.entries(req.output)) {
+                    //         let s = key + ': '
+                    //         for (let i = 0; i < value.length; i++) {
+                    //             s += value[i] + ', '
+                    //         }
+                    //         console.log(s.substring(0, s.length - 2))
+                    //     }
+                    //     console.log("---------------------------")
+                    //     return res.status(200).send('Information logged to the console.')
+                    // }
                 })
             })
         }
     }
+}
 
+const logInformationCSV = (req, res) => {
+    if (isEmptyObject(req.output) === true) {
+        return res.status(400).send('You are using the same ID with different names!')
+    } else {
+        for (const [key, value] of Object.entries(req.output)) {
+            let s = key + ': '
+            for (let i = 0; i < value.length; i++) {
+                s += value[i] + ', '
+            }
+            console.log(s.substring(0, s.length - 2))
+        }
+        console.log("---------------------------")
+        return res.status(200).send('Information logged to the console.')
+    }
+}
+
+const checkObjectsForIdsAndNames = (req, objForCheck, entry) => {
+    for (const [key, value] of Object.entries(objForCheck)) {
+        if (entry.ID === value && entry.FamilyName + ' ' + entry.GivenName !== key) {
+            req.sameIdDifNameFlag = true;
+        }
+    }
+}
+
+const populateOutput = (req, user, entry) => {
+    if (req.sameIdDifNameFlag !== true) {
+        if (entry.Active === 'true' && user.facility.includes(entry.FacilityId)) {
+            if (req.output[entry.FamilyName + ' ' + entry.GivenName]) {
+                req.output[entry.FamilyName + ' ' + entry.GivenName].push(entry.NameId);
+            } else {
+                req.output[entry.FamilyName + ' ' + entry.GivenName] = [entry.NameId];
+            }
+        }
+    }
 }
 
 exports.handleLogin = (req, res) => {
@@ -122,7 +169,7 @@ exports.handleLogin = (req, res) => {
         'authenticated': true,
         'facility': ['12', '13'],
         'roles': [
-            'Admin'
+            'Admi'
         ]
     }
 
